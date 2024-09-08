f274d4084853dc99cd285435a8f9b3a79ca5a879
Dipankar Mnadal <dipankar0535@gmail.com>
2024-09-08 22:08:26 +0530

    My first counter update code

--- Projects/nomal-react/src/App.js
+++ Projects/nomal-react/src/App.js
@@ -1,36 +1,47 @@
+import { useState } from "react"
+import './App.css';
+
 
 
 export default function App() {
 
-  // let ShowDetails = () => {
-  //   console.log("Hi i am Dipankar Mandal")
-  // }
-  // let removeDetails = () => {
-  //   console.log("Hi i am Dipankar Mandal2")
-  // }
-
-  let counter = 10
+  let [counter, setCounter] = useState(0)
+  let [status, setStatus] = useState("")
 
   const AddValue = () => {
-    console.log("Dipankar's rank is :",counter,"|",Math.random())
-    counter++
+    counter = counter+1
+    status = ""
+    setStatus(setStatus)
+    setCounter(counter)
+    console.log("Dipankar's rank is : ",counter,"|",Math.random())
   }
 
+  const RemoveValue = () =>{
+    if (counter > 0)
+    {
+      counter = 0
+      setCounter(counter)
+      console.log("Dipankar's rank is set to: ",{counter})
+    }
+    else{
+      status = "The rank is already 0."
+      setStatus(status)
+      console.log("The rank is already 0.")
+    }
+
+  }
 
   return (
     <>
-      <center>
-        <h1>Hi, I am Dipankar Mandal and my rank is :{counter}</h1>
-
+      <div className="App">
+        <h1 style={{color:"green", fontFamily:"cursive"}}>Hi, I am Dipankar Mandal and my rank is : {counter}</h1>
+        <h1 style={{color:"red", fontFamily:"cursive"}}>{status}</h1>
         <button onClick={AddValue}>AddValue {counter} </button>
         <br/>
         <br/>
         <br/>
-        <button>RemoveValue</button>
-
-        {/* <button onClick={ShowDetails}>Show the Details</button>
-        <button onClick={removeDetails}>Remove the Details</button> */}
-      </center>
+        <button onClick={RemoveValue}>RemoveValue</button>
+      </div >
 
     </>
   );
