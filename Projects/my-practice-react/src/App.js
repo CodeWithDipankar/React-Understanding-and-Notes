7660dedecb9bad211f34e2b712b0713f98254332
Dipankar Mnadal <dipankar0535@gmail.com>
2024-09-11 23:45:03 +0530

    basic changes for new website

--- Projects/my-practice-react/src/App.js
+++ Projects/my-practice-react/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+    <div class= "container">
+      <header class= "header">
+        <nav class= "nav">
+          <ul>
+            <li><Link to="/">Profile</Link></li>
+            <li><Link to="/about">About Me</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+          </ul>
+
+        </nav>
+
       </header>
     </div>
+    </Router>
   );
 }
 
